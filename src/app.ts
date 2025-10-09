a5750399526edab37f2ec692b592808bba0437ef
naeemul-online <naeemul.online@gmail.com>
2025-10-09 10:23:56 +0300

    final updated

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,15 @@ import { userRouter } from "./modules/user/user.routes";
 const app = express();
 
 // Middleware
-app.use(cors()); // Enables Cross-Origin Resource Sharing
-app.use(compression()); // Compresses response bodies for faster delivery
+app.use(cors());
+app.use(compression());
 app.use(cookieParser());
-app.use(express.json()); // Parse incoming JSON requests
+app.use(express.json());
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
-    // credentials: true,
   })
 );
 
